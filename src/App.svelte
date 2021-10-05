c8fc814607d7c445491818d2ca7d4788ed894870
澄三彩 <634725355@qq.com>
2021-10-05 16:59:46 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Anime from "./AppleAnime.svelte";
+</script>
+
+<main>
+	<Anime></Anime>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
